8cffbb82ffbc3fe0e60abafad8fb6aa714811791
Toci <tocirml2@gmail.com>
2021-03-07 18:56:44 -0600

    Add heroku script

--- tools/server.js
+++ tools/server.js
@@ -16,5 +16,5 @@ app.get('/', function (req, res) {
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, () =>
-  console.log('Server is running on http://localhost:3000')
+  console.log('Server is running on http://localhost:8080')
 );
